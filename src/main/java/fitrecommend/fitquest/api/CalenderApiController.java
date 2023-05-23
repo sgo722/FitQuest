df393007cc36f29a923f65939867300ebed3097f
sgo722 <sgo722@naver.com>
2023-05-23 23:17:02 +0900

    CalenderApiController 구성 완료

---
+++ src/main/java/fitrecommend/fitquest/api/CalenderApiController.java
@@ -0,0 +1,75 @@
+package fitrecommend.fitquest.api;
+
+import fitrecommend.fitquest.domain.*;
+import fitrecommend.fitquest.repository.GymReportJPARepository;
+import fitrecommend.fitquest.repository.HomeReportJPARepository;
+import fitrecommend.fitquest.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class CalenderApiController {
+    private final MemberRepository memberRepository;
+    private final GymReportJPARepository gymReportJPARepository;
+    private final HomeReportJPARepository homeReportJPARepository;
+
+    @GetMapping("/calender/{memberId}")
+    public ResponseEntity<ViewCalenderResponseDto> ViewCalender(@PathVariable Long memberId){
+        ViewCalenderResponseDto viewCalenderResponseDto = new ViewCalenderResponseDto();
+        Member member = memberRepository.findOne(memberId);
+        for(GymReport gymReport : member.getGymReports()){
+            GymReportDto gymReportDto = new GymReportDto();
+            gymReportDto.endTime = gymReport.getEndtime();
+            for(Exercise exercise : gymReport.getExercises()){
+                ExerciseDto exerciseDto = new ExerciseDto();
+                exerciseDto.gymName = exercise.getGym().getName();
+                exerciseDto.reps = 0;
+                for(Set set : exercise.getSets()) {
+                    exerciseDto.reps += set.getRep();
+                }
+                gymReportDto.exerciseDtos.add(exerciseDto);
+            }
+            viewCalenderResponseDto.gymReports.add(gymReportDto);
+        }
+
+        for(HomeReport homeReport : member.getHomeReports()){
+            HomeReportDto homeReportDto = new HomeReportDto();
+            homeReportDto.endTime = homeReport.getEndtime();
+            homeReportDto.url = homeReport.getHome().getUrl();
+            viewCalenderResponseDto.homeReports.add(homeReportDto);
+        }
+        return ResponseEntity.ok(viewCalenderResponseDto);
+    }
+
+    public class ViewCalenderResponseDto{
+        private List<GymReportDto> gymReports;
+        private List<HomeReportDto> homeReports;
+    }
+
+    public class GymReportDto{
+        private LocalDateTime endTime;
+        private List<ExerciseDto> exerciseDtos; // 운동종류들
+    }
+
+    public class ExerciseDto{
+        private String gymName; // 어떤 운동을 했는지
+        private Integer reps; // 총 수행횟수
+    }
+
+    public class HomeReportDto{
+        private LocalDateTime endTime;
+        private String url;
+
+    }
+
+
+}
